e10c6760f65a15e6e0cb80b2ec1b90ab8547d987
RemoteEvilDragon <fight.or.fighter@gmail.com>
2016-01-04 23:56:17 +0800

    first commit

---
+++ print.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var (
+    version  = "0.8"
+    build    = "Custom"
+    codename = "Post Apocalypse"
+    intro    = "A stable and unbreakable connection for everyone."
+)
+
+func main() {
+    fmt.Printf("hello, world:%v\n",version)
+}
