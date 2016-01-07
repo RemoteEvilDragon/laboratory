a4fdf0bc4532dbfd598b25c71baa6ac0a9722ae8
Athen_King <fight.or.fighter@gmail.com>
2016-01-07 19:06:49 +0800

    add table to json clauses

--- lua/test.lua
+++ lua/test.lua
@@ -1,52 +1,56 @@
-function orderedTable2json(t)
-    local function serialize(tbl)
-        local tmp = {}
-        local count = 0
-        for _,t in ipairs(tbl) do
-            for k, v in pairs(t) do
-                local k_type = type(k)
-                local v_type = type(v)
+function table2json( list )
+    local json =  "{"
 
+    local function iterFun (tab)
+        for i,v in ipairs(tab) do
+            if type(v) == "table" then--means key value. or .value list
+                -- table.combine(tmp,iterFun(v))
+                for k,t in pairs(v) do
+                    if type(k) == "number" then
+                        --means value list
+                        if k == 1 then
+                            json = json .."["
+                        end
 
-                local key = (k_type == "string" and "\"" .. k .. "\":")
-                    or (k_type == "number" and "")
+                        json = json ..tostring(t)
 
-                local value = (v_type == "table" and serialize(v))
-                    or (v_type == "boolean" and tostring(v))
-                    or (v_type == "string" and "\"" .. v .. "\"")
-                    or (v_type == "number" and v)
-
-                tmp[#tmp + 1] = key and value and tostring(key) .. tostring(value) or nil
+                        if k == #v then
+                            json = json .."]"
+                        end
+                    else
+                        --means key value
+                        json = json..k..":"
+                        if type(v) == "table" then
+                            iterFun(v)
+                        else
+                            json = json..tostring(v)
+                        end
+                    end
+                end
+            else
+                json = json .. tostring(v)
             end
-            count = count + 1
-        end
 
-        if count == 0 then
-            return "{" .. table.concat(tmp, ",") .. "}"
-        else
-            return "[" .. table.concat(tmp, ",") .. "]"
+            json = json.. ((i == #tab) and "," or "")
         end
     end
-    assert(type(t) == "table")
-    return serialize(t)
+
+    iterFun(list)--returns json element.
+
+
+    -- table.insert(json,iterFun(list))
+
+    -- return "{" .. table.concat(json, ",") .. "}"
+    return json .. "}"
 end
 
---this is a bad method,not good for iterating.
-local k_m = {
-{le = { {level=1},{b=3} }  },
--- {le = 1  },
-{cu2=2},
-{combo=3},
-{perfect=4}
-}
 
--- local k_m = {a_le=1,b_lee=1,c_cu2=2,d_combo=3,e_perfect=4}
-print(orderedTable2json(k_m))
-
--- for _,t in ipairs(k_m) do
---     for k,v in pairs(t) do
---     	print(k)
---     	print("\n")
---     	print(v)
---     end
--- end
+local jsonString = table2json(
+                {
+                {shaderName="customTest"},
+                {vert="res/shaders/test_vert.c"},
+                {frag="res/shaders/test_frag.c"},
+                {iResolution = {1080,768,0} }
+                }
+            )
+print("json string is "..jsonString)
