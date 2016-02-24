ca5d2e9d11ad3e2887d0d270d99fa7a4320cdf54
Athen_King <fight.or.fighter@gmail.com>
2016-02-24 19:04:09 +0800

    add a shell script file to upload files wthin a directory

---
+++ shells/athenVps/test.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# test=$1
+
+# echo ${test##*/}
+
+# list=$(ls)
+
+# echo $list
+
+# if [[ -d $1 ]] ; then
+#   echo "$1 is a directory"
+# fi
+
+function iterDir(){
+	for f in $(ls $1) ; do
+		sub="$1/$f"
+		if [[ -d $sub ]] ; then
+			echo "$f is a directory"
+			iterDir "$1/$f"
+		elif [[ -f $sub ]] ; then
+			echo "$f is an file"
+		fi
+	done
+}
+iterDir $1
+
+# filelist=$(ls $1)
+# for f in $filelist ; do
+# 	echo $f
+# 	w="$"
+# 	if [[ -d $f ]] ; then
+# 		echo "$f is a directory"
+# 	elif [[ -f $f ]] ; then
+# 		echo "$f is an file"
+# 	fi
+# done
--- shells/athenVps/uploadToAthen.sh
+++ shells/athenVps/uploadToAthen.sh
@@ -1,15 +1,44 @@
 #!/bin/sh
-
 USERNAME="athenking"
 PASSWORD="freedom"
 SERVER="27.126.181.90"
-DIR=$1
-FILE=$2
 
-cd $DIR
+# CLIENTDIR=$1
+SERVERDIR=""
+
+if [[ -d $1 ]]; then
+  SERVERDIR=${1##*/}
+fi
+
+declare -a list
+declare -i count=0
+
+function copy_file (){
+	for f in $(ls $1) ; do
+		sub="$1/$f"
+		if [[ -d $sub ]]; then
+			subDir=${f##*/}
+			list[$count]="mkdir $f"
+			count=$count+1
+
+			list[$count]="cd $f"
+			count=$count+1
+			SERVERDIR="$SERVERDIR/$f"
+
+			copy_file $sub
+		elif [[ -f $sub ]]; then
+			list[$count]="put $sub $SERVERDIR/$f"
+			count=$count+1
+		fi
+	done
+}
+
+copy_file $1
+
+# echo "${list[*]}"
 ftp -n -i $SERVER <<EOF
-user $USERNAME $PASSWORD
-binary
-put "$DIR/$FILE" $FILE
-exit
+	user $USERNAME $PASSWORD
+	binary
+	${list[*]}
+	exit
 EOF
