bb35dd2c5165ce4d85cb3dada693fe97369392c6
Athen_King <fight.or.fighter@gmail.com>
2016-03-07 10:29:25 +0800

    c

--- iospacking/_scripts/build.py
+++ iospacking/_scripts/build.py
@@ -21,8 +21,8 @@ filepath = sys.path[0]
 from PTlib.utils import *
 
 
-is_Release_InHouse = False
-# is_Release_InHouse = True
+# is_Release_InHouse = False
+is_Release_InHouse = True
 
 # 首先读取配置文件
 if not is_Release_InHouse:
